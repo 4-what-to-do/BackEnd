baec263bcac3adcabfd8ace6c76fae3f073fdd9b
hunny-A <nahyun7656a@gmail.com>
2023-02-28 01:43:57 +0900

    Feat : test2

--- src/main/java/com/sparta/todo/service/LikeService.java
+++ src/main/java/com/sparta/todo/service/LikeService.java
@@ -22,7 +22,6 @@ public class LikeService {
 
 
     /* Post(하루 일정) 좋아요 기능 */
-
     @Transactional
     public ResponseEntity<SuccessMessageDto> likePost(Long postId, User user) {
         Optional<Post> post = postRepository.findById(postId);
